359f8f66262faf520d14f87e5a356f4e2317dd40
Hussain <hkosaimi@gmail.com>
2023-09-14 17:08:48 +0300

    new

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,34 @@
 import "./Home.css";
 import { motion } from "framer-motion";
 import Professors from "../../components/Professors/Professors";
-
+import Box from "../../assets/box.png";
 function Home() {
   return (
     <>
       <div className="home">
         <div className="home__top">
           <div className="home__top__first">
-            <h1>Built by students for students.</h1>
-            <p>
-              Auknotes is a community for students to help them success during
-              their college life. Only AUK'res can view and participate, no
-              professors or tutors. Our journey just started.
-            </p>
-            <button>Explore</button>
+            <div className="home__left">
+              <h1>Built by students for students.</h1>
+              <p>
+                Auknotes is a community for students to help them success during their college life.
+                Only AUK'res can view and participate, no professors or tutors. Our journey just
+                started.
+              </p>
+              <button>Explore</button>
+            </div>
+            <div className="home__right">
+              <img src={Box} />
+            </div>
           </div>
           <div className="home__top__second">
             <div className="home__top__second--card-container">
               <div className="card">
                 <h2>Use our website to enhance your learning process.</h2>
                 <p>
-                  Before you can view and participate in the website, you need
-                  to signup with you AUK email, to ensure that only students can
-                  have access to experince the full, unlimited features.
+                  Before you can view and participate in the website, you need to signup with you
+                  AUK email, to ensure that only students can have access to experince the full,
+                  unlimited features.
                 </p>
                 <div className="input-container">
                   <input type="email" placeholder="Email"></input>
@@ -36,10 +41,7 @@ function Home() {
 
         <div className="home__middle">
           <div className="home__middle-top">
-            <h1>
-              We're providing a variety of resources to guide you throughout
-              your semester
-            </h1>
+            <h1>We're providing a variety of resources to guide you throughout your semester</h1>
           </div>
           <div className="courses_container">
             <motion.div
